d77d4218e8ebf4490ad9a5c73120f4cfb7ae70e5
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2016-11-26 19:48:04 +0100

    Add eslint

--- build.js
+++ build.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const path = require('path')
 const cssscss = require('css-scss')
 const postcss = require('postcss')
 const postcssImport = require('postcss-import')
@@ -11,31 +10,31 @@ const filename = 'src/material-utilities.css'
 let css = fs.readFileSync(filename, 'utf8')
 
 postcss([postcssImport, postcssCustomProperties, autoprefixer, cssnano])
-    .process(css, {
-        from: filename,
-        to: '/dist/material-utilities.min.css'
-    })
-    .then(function(result) {
-        fs.writeFileSync('./dist/material-utilities.min.css', result.css);
-        if (result.map) fs.writeFileSync('material-utilities.min.css.map', result.map);
-    });
+  .process(css, {
+    from: filename,
+    to: 'dist/material-utilities.min.css'
+  })
+  .then((result) => {
+    fs.writeFileSync('dist/material-utilities.min.css', result.css)
+    if (result.map) fs.writeFileSync('material-utilities.min.css.map', result.map)
+  })
 
 postcss([postcssImport, postcssCustomProperties, autoprefixer])
-    .process(css, {
-        from: filename,
-        to: 'material-utilities.css'
-    })
-    .then(function(result) {
-        fs.writeFileSync('./dist/material-utilities.css', result.css);
-        if (result.map) fs.writeFileSync('./dist/material-utilities.css.map', result.map);
-    });
+  .process(css, {
+    from: filename,
+    to: 'material-utilities.css'
+  })
+  .then((result) => {
+    fs.writeFileSync('dist/material-utilities.css', result.css)
+    if (result.map) fs.writeFileSync('dist/material-utilities.css.map', result.map)
+  })
 
 const modules = [
-    'classes',
-    'variables'
+  'classes',
+  'variables'
 ]
 
-for (module of modules) {
-    let file = fs.readFileSync('./src/' + module + '.css', 'utf8')
-    fs.writeFileSync('./dist/' + module + '.scss', cssscss(file));
+for (let module of modules) {
+  const file = fs.readFileSync('src/' + module + '.css', 'utf8')
+  fs.writeFileSync('dist/' + module + '.scss', cssscss(file))
 }
